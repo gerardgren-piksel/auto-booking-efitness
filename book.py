e2c6602b24c872a6526714aa545fef208f5faa41
gerardgren-piksel <gerardgren@gmail.com>
2026-05-21 20:07:26 +0200

    Update book.py

--- book.py
+++ book.py
@@ -1,2 +1,11 @@
-requests
+import os
+
+login = os.getenv("EFITNESS_LOGIN")
+password = os.getenv("EFITNESS_PASSWORD")
+
+if not login or not password:
+    raise SystemExit("Brak sekretów EFITNESS_LOGIN lub EFITNESS_PASSWORD")
+
+print("Sekrety są dostępne. Następny krok: logowanie i zapis.")
+
 
