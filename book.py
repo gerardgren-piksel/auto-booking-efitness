de48538997dda2a2c11fabed2449c4cf321dc7b0
gerardgren-piksel <gerardgren@gmail.com>
2026-05-23 07:11:51 +0200

    Update book.py

--- book.py
+++ book.py
@@ -2,12 +2,11 @@ import os
 from pathlib import Path
 from playwright.sync_api import sync_playwright
 
+BASE_URL = "https://cf43300-cms.efitness.com.pl"
+LOGIN_URL = BASE_URL
+
 LOGIN = os.getenv("EFITNESS_LOGIN", "")
 PASSWORD = os.getenv("EFITNESS_PASSWORD", "")
-page.goto(LOGIN_URL, wait_until="networkidle")
-page.get_by_text("Zaloguj się", exact=False).click()
-
-
 
 out = Path("output")
 out.mkdir(exist_ok=True)
@@ -16,11 +15,13 @@ def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto(LOGIN_URL, wait_until="networkidle")
 
-        out.joinpath("login_debug.html").write_text(page.content(), encoding="utf-8")
-        out.joinpath("login_debug.txt").write_text(page.locator("body").inner_text(), encoding="utf-8")
-        page.screenshot(path=str(out / "login_debug.png"), full_page=True)
+        page.goto(LOGIN_URL, wait_until="networkidle")
+        page.get_by_text("Zaloguj się", exact=False).click()
+
+        out.joinpath("debug.html").write_text(page.content(), encoding="utf-8")
+        out.joinpath("debug.txt").write_text(page.locator("body").inner_text(), encoding="utf-8")
+        page.screenshot(path=str(out / "debug.png"), full_page=True)
 
         browser.close()
 
