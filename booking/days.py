09587b45760b2372b4bbaaeaecb05a7506a0a50a
gerardgren-piksel <gerardgren@gmail.com>
2026-07-03 16:59:08 +0200

    Create days.py

---
+++ booking/days.py
@@ -0,0 +1,28 @@
+DAY_MAP = {
+    "MONDAY": "PONIEDZIAŁEK",
+    "TUESDAY": "WTOREK",
+    "WEDNESDAY": "ŚRODA",
+    "THURSDAY": "CZWARTEK",
+    "FRIDAY": "PIĄTEK",
+    "SATURDAY": "SOBOTA",
+    "SUNDAY": "NIEDZIELA",
+    "PONIEDZIAŁEK": "PONIEDZIAŁEK",
+    "WTOREK": "WTOREK",
+    "ŚRODA": "ŚRODA",
+    "SRODA": "ŚRODA",
+    "CZWARTEK": "CZWARTEK",
+    "PIĄTEK": "PIĄTEK",
+    "PIATEK": "PIĄTEK",
+    "SOBOTA": "SOBOTA",
+    "NIEDZIELA": "NIEDZIELA",
+}
+
+PL_DAY_BY_WEEKDAY = {
+    0: "PONIEDZIAŁEK",
+    1: "WTOREK",
+    2: "ŚRODA",
+    3: "CZWARTEK",
+    4: "PIĄTEK",
+    5: "SOBOTA",
+    6: "NIEDZIELA",
+}
